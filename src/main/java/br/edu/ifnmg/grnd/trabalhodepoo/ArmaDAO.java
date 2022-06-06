8d38cb294788c3e1de1841264f2610acaa809d87
Lucas Flávio <lucasfgm00@gmail.com>
2022-06-06 01:21:44 -0300

    docs lucas

--- src/main/java/br/edu/ifnmg/grnd/trabalhodepoo/ArmaDAO.java
+++ src/main/java/br/edu/ifnmg/grnd/trabalhodepoo/ArmaDAO.java
@@ -11,72 +11,95 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Operações concretas que suportam os procedimentos CRUD em objetos em banco de
+ * dados.
  *
  * @author gusta
  */
-public class ArmaDAO extends Dao <Arma, Long> {
-                    @Override
-	public String obterSentencaInsert() {
-		return "insert into arma(alcance_effective, alcance_max, dano_roll, dano_dice, idItemGeral) values (?, ?, ?, ?, ?);";
-	}
+public class ArmaDAO extends Dao<Arma, Long> {
 
-	@Override
-	public String obterSentencaUpdate() {
-		return "update arma set alcance_effective = ?, alcance_max = ?, dano_roll = ?, dano_dice = ?, idItemGeral = ? where id = ?;";
-	}
+    /*
+    CREATE TABLE arma (
+        `alcance_effective` int,
+        `alcance_max` int,
+        `dano_roll` int,
+        `dano_dice` int,
+        `id` int AUTO_INCREMENT,
+        PRIMARY KEY (`id`),
+        `idItemGeral` int,
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1
+-- */
+    @Override
+    public String obterSentencaInsert() {
+        return "insert into arma(alcance_effective, alcance_max, dano_roll, dano_dice, idItemGeral) values (?, ?, ?, ?, ?);";
+    }
 
-	@Override
-	 public String obterSentencaLocalizarPorId() {
-		return "select id, alcance_effective, alcance_max, dano_roll, dano_dice, idItemGeral from arma where id = ?;";
-	 }
+    @Override
+    public String obterSentencaUpdate() {
+        return "update arma set alcance_effective = ?, alcance_max = ?, dano_roll = ?, dano_dice = ?, idItemGeral = ? where id = ?;";
+    }
 
-	@Override
-	public String obterSentencaLocalizarTodos() {
-		return "select id, alcance_effective, alcance_max, dano_roll, dano_dice, idItemGeral from arma where excluido = false;";
-	 }
-    
-    
-	@Override
-	public void montarDeclaracao(PreparedStatement pstmt, Arma e) {
-		try {
-		    pstmt.setInt(1, e.getAlcance().get(0));
-		    pstmt.setInt(2, e.getAlcance().get(1));
-		    pstmt.setInt(3, e.getDano().get(0));
-                                                pstmt.setInt(4, e.getDano().get(1));
-		    pstmt.setLong(5, new ItemGeralDAO().salvar(e) );
+    @Override
+    public String obterSentencaLocalizarPorId() {
+        return "select id, alcance_effective, alcance_max, dano_roll, dano_dice, idItemGeral from arma where id = ?;";
+    }
 
-		    if (e.getId() != null && e.getId() != 0) {
-			pstmt.setLong(7, e.getId());
-		    }
+    @Override
+    public String obterSentencaLocalizarTodos() {
+        return "select id, alcance_effective, alcance_max, dano_roll, dano_dice, idItemGeral from arma where excluido = false;";
+    }
 
-		} catch (Exception ex) {
-		    System.out.println("Exceptoin: " + ex);
-		}
-	    }
+    /**
+     * Substitui elementos variáveis na SQL preparada a partir do objeto de
+     * referência recebido
+     *
+     * @param pstmt Consulta preparada com valores ausentes
+     * @param e Objeto com dados relevantes para a consulta
+     */
+    @Override
+    public void montarDeclaracao(PreparedStatement pstmt, Arma e) {
+        try {
+            pstmt.setInt(1, e.getAlcance().get(0));
+            pstmt.setInt(2, e.getAlcance().get(1));
+            pstmt.setInt(3, e.getDano().get(0));
+            pstmt.setInt(4, e.getDano().get(1));
+            pstmt.setLong(5, new ItemGeralDAO().salvar(e));
 
+            if (e.getId() != null && e.getId() != 0) {
+                pstmt.setLong(7, e.getId());
+            }
 
-	    @Override
-	    public Arma extrairObjeto(ResultSet resultSet) {
-		Arma e = new Arma();
+        } catch (Exception ex) {
+            System.out.println("Exceptoin: " + ex);
+        }
+    }
 
-		try {
-                                                        e.setAlcance(resultSet.getInt("alcance_effective"), resultSet.getInt("alcance_max"));
-                                                        e.setDano(resultSet.getInt("dano_roll"), resultSet.getInt("dano_dice"));
-                                                        ItemGeral aux =  new ItemGeralDAO().localizarPorId(resultSet.getLong("idItemGeral"));
-                                                        e.setDescricao(aux.getDescricao());
-                                                        e.setNome(aux.getNome());
-                                                        e.setPreco(aux.getPreco());
-                                                        e.setPeso(aux.getPeso());
-                                                        e.setEquipado(aux.getEquipado());
-                                                        e.setQuantidade(aux.getQuantidade());
+    /**
+     * Extrai um objeto Arma do resultado gerado pela consulta
+     *
+     * @param resultSet Registro recuperado do banco de dados
+     * @return Arma equivalente ao registro recebido
+     */
+    @Override
+    public Arma extrairObjeto(ResultSet resultSet) {
+        Arma e = new Arma();
 
+        try {
+            e.setAlcance(resultSet.getInt("alcance_effective"), resultSet.getInt("alcance_max"));
+            e.setDano(resultSet.getInt("dano_roll"), resultSet.getInt("dano_dice"));
+            ItemGeral aux = new ItemGeralDAO().localizarPorId(resultSet.getLong("idItemGeral"));
+            e.setDescricao(aux.getDescricao());
+            e.setNome(aux.getNome());
+            e.setPreco(aux.getPreco());
+            e.setPeso(aux.getPeso());
+            e.setEquipado(aux.getEquipado());
+            e.setQuantidade(aux.getQuantidade());
 
-		} catch (SQLException ex) {
-		    Logger.getLogger(ItemGeral.class.getName()).log(Level.SEVERE, null, ex);
-		}
+        } catch (SQLException ex) {
+            Logger.getLogger(ItemGeral.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-		return e;
-	    }
+        return e;
+    }
 
-    
 }
--- src/main/java/br/edu/ifnmg/grnd/trabalhodepoo/ArmaduraDAO.java
+++ src/main/java/br/edu/ifnmg/grnd/trabalhodepoo/ArmaduraDAO.java
@@ -11,69 +11,92 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Operações concretas que suportam os procedimentos CRUD em objetos em banco de
+ * dados.
  *
  * @author gusta
  */
-public class ArmaduraDAO extends Dao <Armadura, Long>{
-     @Override
-	public String obterSentencaInsert() {
-		return "insert into armadura(tipo, defesa, idItemGeral)  values (?, ?, ?);";
-	}
+public class ArmaduraDAO extends Dao<Armadura, Long> {
 
-	@Override
-	public String obterSentencaUpdate() {
-		return "update armadura set tipo = ?, defesa = ?, idItemGeral = ? where id = ?;";
-	}
+    /*
+    CREATE TABLE armadura (
+        `tipo` varchar(255),
+        `defesa` int,
+        `id` int AUTO_INCREMENT,
+        PRIMARY KEY (`id`),
+        `idItemGeral` int,
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1
+-- */
+    @Override
+    public String obterSentencaInsert() {
+        return "insert into armadura(tipo, defesa, idItemGeral)  values (?, ?, ?);";
+    }
 
-	@Override
-	 public String obterSentencaLocalizarPorId() {
-		return "select id, tipo, defesa, idItemGeral from armadura where id = ?;";
-	 }
+    @Override
+    public String obterSentencaUpdate() {
+        return "update armadura set tipo = ?, defesa = ?, idItemGeral = ? where id = ?;";
+    }
 
-	@Override
-	public String obterSentencaLocalizarTodos() {
-		return "select id, tipo, defesa, idItemGeral from armadura where excluido = false;";
-	 }
-    
-    
-	@Override
-	public void montarDeclaracao(PreparedStatement pstmt, Armadura e) {
-		try {
-		    pstmt.setString(1, e.getTipo());
-		    pstmt.setInt(2, e.getDefesa());
-		    pstmt.setLong(3, new ItemGeralDAO().salvar(e) );
+    @Override
+    public String obterSentencaLocalizarPorId() {
+        return "select id, tipo, defesa, idItemGeral from armadura where id = ?;";
+    }
 
-		    if (e.getId() != null && e.getId() != 0) {
-			pstmt.setLong(7, e.getId());
-		    }
+    @Override
+    public String obterSentencaLocalizarTodos() {
+        return "select id, tipo, defesa, idItemGeral from armadura where excluido = false;";
+    }
 
-		} catch (Exception ex) {
-		    System.out.println("Exceptoin: " + ex);
-		}
-	    }
+    /**
+     * Substitui elementos variáveis na SQL preparada a partir do objeto de
+     * referência recebido
+     *
+     * @param pstmt Consulta preparada com valores ausentes
+     * @param e Objeto com dados relevantes para a consulta
+     */
+    @Override
+    public void montarDeclaracao(PreparedStatement pstmt, Armadura e) {
+        try {
+            pstmt.setString(1, e.getTipo());
+            pstmt.setInt(2, e.getDefesa());
+            pstmt.setLong(3, new ItemGeralDAO().salvar(e));
 
+            if (e.getId() != null && e.getId() != 0) {
+                pstmt.setLong(7, e.getId());
+            }
 
-	    @Override
-	    public Armadura extrairObjeto(ResultSet resultSet) {
-		Armadura e = new Armadura();
+        } catch (Exception ex) {
+            System.out.println("Exceptoin: " + ex);
+        }
+    }
 
-		try {
-                                                        e.setTipo(resultSet.getString("tipo"));
-                                                        e.setDefesa(resultSet.getInt("defesa"));
-                                                        ItemGeral aux =  new ItemGeralDAO().localizarPorId(resultSet.getLong("idItemGeral"));
-                                                        e.setDescricao(aux.getDescricao());
-                                                        e.setNome(aux.getNome());
-                                                        e.setPreco(aux.getPreco());
-                                                        e.setPeso(aux.getPeso());
-                                                        e.setEquipado(aux.getEquipado());
-                                                        e.setQuantidade(aux.getQuantidade());
+    /**
+     * Extrai um objeto Arma do resultado gerado pela consulta
+     *
+     * @param resultSet Registro recuperado do banco de dados
+     * @return Arma equivalente ao registro recebido
+     */
 
+    @Override
+    public Armadura extrairObjeto(ResultSet resultSet) {
+        Armadura e = new Armadura();
 
-		} catch (SQLException ex) {
-		    Logger.getLogger(ItemGeral.class.getName()).log(Level.SEVERE, null, ex);
-		}
+        try {
+            e.setTipo(resultSet.getString("tipo"));
+            e.setDefesa(resultSet.getInt("defesa"));
+            ItemGeral aux = new ItemGeralDAO().localizarPorId(resultSet.getLong("idItemGeral"));
+            e.setDescricao(aux.getDescricao());
+            e.setNome(aux.getNome());
+            e.setPreco(aux.getPreco());
+            e.setPeso(aux.getPeso());
+            e.setEquipado(aux.getEquipado());
+            e.setQuantidade(aux.getQuantidade());
 
-		return e;
-	    }
-    
+        } catch (SQLException ex) {
+            Logger.getLogger(ItemGeral.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return e;
+    }
+
 }
